1632034e550dfab62ca15328eab61965354e8657
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-09-17 22:39:35 -0300

    build: Ajustar dependencia do flyway para suporte ao postgresql

--- src/main/java/unicap/juryscan/infra/security/SecurityConfiguration.java
+++ src/main/java/unicap/juryscan/infra/security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package unicap.juryscan.security;
+package unicap.juryscan.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
